0fd6cb68aa00eb1145c596f600eb378bd4fc2079
ISC Tesoem <tesoem@iMac-Tesoem-01.local>
2020-01-07 13:09:14 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/txtarchivossccm/ManejoArchivo.java
@@ -0,0 +1,18 @@
+package mx.edu.tesoem.isc.txtarchivossccm;
+
+import android.content.Context;
+
+public class ManejoArchivo {
+
+
+    public boolean grabar(Context context, String dato){
+        try{
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+
+        }
+        return true;
+    }
+}
